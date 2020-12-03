d6abeaef4a507073acf53c3a176d0a34d3c09605
Kos <locoprodukten@gmail.com>
2020-12-03 11:17:44 +0100

    Added About page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import "./App.css";
+import React from "react";
+
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
+
 import MyNavbar from "./components/my-navbar/mynavbar.component";
 import MyCarousel from "./components/my-carousel/my-carousel.component";
 import TitleMessage from "./components/title-message/title-message.component";
+import About from "./pages/about/about.component";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="App" style={{ position: "relative" }}>
       <MyNavbar />
       <MyCarousel />
       <TitleMessage />
+
+      <div>
+        <Parallax
+          blur={{ min: -1000, max: 1000 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <About />
+            </Fade>
+          </Container>
+        </Parallax>
+      </div>
     </div>
   );
 };
