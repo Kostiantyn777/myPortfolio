e5c8816feb9edfa70d37f1c7b3ae27cd6e1e6c7e
Kos <locoprodukten@gmail.com>
2021-04-02 19:16:34 +0200

    Testing 01

--- src/components/title-message/title-message.component.jsx
+++ src/components/title-message/title-message.component.jsx
@@ -33,7 +33,7 @@ const TitleMessage = () => {
       <div className="titleMessage">
         <div className="heading">
           <div className="main text-center mb-3">
-            Hi, I am
+            hello what are you doing......., I am
             <br />
             <span>
               <strong>Kostiantyn Borodach</strong>
